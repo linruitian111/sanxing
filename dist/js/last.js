d95ce3a08343dc065a7a39601a72bfd24d1c5e48
linruitian <1906067374@qq.com>
2020-07-07 20:32:17 +0800

    last

---
+++ dist/js/last.js
@@ -0,0 +1,1 @@
+"use strict";console.log(111),console.log($(".swiper-wrapper button")),$(".swiper-wrapper #first").on("click",function(){console.log(111),$("swiper-wrapper ul ").stop().animate({top:73*$(this).index()})});
